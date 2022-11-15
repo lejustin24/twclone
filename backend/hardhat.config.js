04c659ef8d18df940f4416c78f4e985f6cb27682
Justin <mrjustin24@outlook.com>
2022-11-15 15:00:47 +0100

    modified:   backend/hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -6,9 +6,11 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: `${process.env.ALCHEMY_GOERLI_URL}`,
+      url: `${process.env.ALCHEMY_GOERLI_URL}`, 
       accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     }
   }
 };
 
+// we don't upload the .env file since we have our private key inside it. require('dotenv').config()
+// so we don't have to worry about our private key >>> process.env.ACCOUNT_PRIVATE_KEY .. 
